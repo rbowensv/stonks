4c4dfa0d0618c88234895aa1246e98f1ad9fa9ec
rbowensv <rbowensv@gmail.com>
2021-07-19 10:45:00 -0400

    done for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { ALPHA_VANTAGE_API, ALPHA_VANTAGE_ROUTE, APP_DESCRIPTION, APP_TITLE } from './app-constants';
+import { StockSearch } from './StockSearch';
+import { useStockSearch } from './useStockSearch';
+import axios from 'axios';
+import { StockView } from './StockView';
+
+export const axiosInstance = axios.create({baseURL: ALPHA_VANTAGE_ROUTE, params: {apikey: ALPHA_VANTAGE_API}});
 
 function App() {
+  const {
+    viewedStocks,
+    searchSuggestions,
+    getSearchSuggestion,
+    pushStock,
+    clearSearchSuggestions,
+    removeStock
+  } = useStockSearch();
+
+  const stockSearchProps = {
+      searchSuggestions,
+      getSearchSuggestion,
+      pushStock,
+      clearSearchSuggestions,
+      viewedStocks
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {APP_TITLE}
+        <div className="App-header--description">
+          {APP_DESCRIPTION}
+        </div>
+        <StockSearch {...stockSearchProps}/>
       </header>
+      <div className='stock-compare'>
+        {viewedStocks && viewedStocks.map((stock) => {
+          return <StockView key={stock.symbol} symbol={stock.symbol} name={stock.name} removeStock={removeStock}/>
+        }
+        )}
+      </div>
     </div>
   );
 }
